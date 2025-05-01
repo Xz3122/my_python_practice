c5ed3e8a355050293290cf5042ed99518383bd9e
王俊邦 <st10202095@gmail.com>
2025-05-01 16:13:53 +0800

    guess_number(while-if)

---
+++ guess_number(while-if).py
@@ -0,0 +1,22 @@
+import random
+
+answer = random.randint(1, 100)
+n = int(input("Please enter number 1~100: "))
+
+if n > answer:
+    print("Too large!")
+elif n < answer:
+    print("Too small!")
+else:
+    print("That's right!")
+
+# while 迴圈代表至少執行 0 次，但猜數字至少要執行 1 次
+while n != answer:
+    n = int(input("Please enter number 1~100: "))
+    if n > answer:
+        print("Too large!")
+    elif n < answer:
+        print("Too small!")
+    else:
+        print("That's right!")
+        break
