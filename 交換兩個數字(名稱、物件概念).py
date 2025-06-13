6a72bcfa8cb0c62685cffec628cdb7fc21a3071e
王俊邦 <st10202095@gmail.com>
2025-06-13 17:58:09 +0800

    交換兩個數字

--- 交換兩個數字(名稱、物件概念).py
+++ 交換兩個數字(名稱、物件概念).py
@@ -5,3 +5,11 @@ old_a = a #將a(名稱)附值於old_a(名稱)，讓old_a順著a(名稱)找到inp
 a = b     #將b(名稱)附值於a(名稱)，讓a順著b(名稱)找到input b(物件)
 b = old_a #將old_a(名稱)附值於b(名稱)，讓b順著old_a(名稱)->a(名稱)找到input a(物件)
 print("交換以後a = %d，b = %d"%(a,b))
+
+x = 9//5 
+y = 9%5
+print(x,y)
+z = divmod(9,5)#一次取得商數及餘數
+print(z)
+y, x = z
+print(x,y)
